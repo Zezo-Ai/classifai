41b1c61434fef13f0454eea7561b006390ccb515
Darin Kotter <darin.kotter@gmail.com>
2024-11-21 15:10:50 -0700

    Modify the default prompt a bit

--- includes/Classifai/Features/DescriptiveTextGenerator.php
+++ includes/Classifai/Features/DescriptiveTextGenerator.php
@@ -27,7 +27,7 @@ class DescriptiveTextGenerator extends Feature {
 	 *
 	 * @var string
 	 */
-	public $prompt = 'You are an assistant that generates descriptions of images posted on a website. You will be provided with an image and will describe the main item you see in the image, giving details but staying concise. There is no need to say "the image contains" or similar, just describe what is actually in the image.';
+	public $prompt = 'You are an assistant that generates descriptions of images that are used on a website. You will be provided with an image and will describe the main item you see in the image, giving details but staying concise. There is no need to say "the image contains" or similar, just describe what is actually in the image. This text will be important for screen readers, so make sure it is descriptive and accurate but not overly verbose.';
 
 	/**
 	 * Constructor.
