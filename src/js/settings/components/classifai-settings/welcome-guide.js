7bb3877ef9858d3ef631fcdb2f30a6caba0fbef0
Darin Kotter <darin.kotter@gmail.com>
2024-12-17 10:18:28 -0700

    Fix eslint error

--- src/js/settings/components/classifai-settings/welcome-guide.js
+++ src/js/settings/components/classifai-settings/welcome-guide.js
@@ -2,7 +2,6 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { NavLink } from 'react-router-dom';
 import { Icon, Button, Flex, FlexItem } from '@wordpress/components';
 import { close } from '@wordpress/icons';
 
